201125053e26e4e538e26f6b7d1c4aea558cf6e7
minju <kimminju3335@naver.com>
2023-10-31 16:49:44 +0900

    update 231031 -2

--- app_intro/views.py
+++ app_intro/views.py
@@ -1,6 +1,68 @@
 from django.shortcuts import render
+import random
+from faker import Faker
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
+
+def hello(request):
+    username = '김민주'
+
+    result = {
+        'username': username,
+    }
+
+    return render(request, 'hello.html', result)
+
+
+def dinner(request):
+    menus = ['라면', '김밥', '떡볶이']
+    pick = random.choice(menus)
+
+    result = {
+        'pick': pick,
+    }
+
+    return render(request, 'dinner.html', result)
+
+
+def lotto(request):
+    numbers = range(71)
+    number = random.sample(numbers, 6)
+    result = {
+        'number': number
+    }
+    return render(request, 'lotto.html', result)
+
+
+def greeting(request, name):
+
+    result = {
+        'name': name,
+    }
+
+    return render(request, 'greeting.html', result)
+
+def cube(request, num):
+    result = {
+        'num': num,
+        'cube': int(num)**3,
+    }
+    return render(request, 'cube.html', result)
+
+def posts(request):
+    posts = []
+
+    fake = Faker()
+
+    for i in range(100):
+        posts.append(fake.text())
+
+    result = {
+        'posts':posts,
+    }
+
+    return render(request, 'posts.html', result)
+
